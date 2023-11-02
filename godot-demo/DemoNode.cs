b4f6b5be0193286f0b6ca8375bd4e02cd8e61eab
Scott Cabot <iam@scott.cab>
2023-11-02 15:39:57 +1030

    Tidy up unnecessary code.

--- godot-demo/DemoNode.cs
+++ godot-demo/DemoNode.cs
@@ -3,23 +3,21 @@ using Godot;
 public abstract partial class DemoNode : Node
 {
 	private readonly Color initializingColor = new(1, 0, 0);
-	private readonly int SecondInMilliSecond = 1000;
 	[Export] private Color tintColor;
 
-	protected virtual int InitialWaitTimeInMilliSecs => 0;
-	protected int WaitTime => InitialWaitTimeInMilliSecs + SecondInMilliSecond + ((int)(GD.Randf() * 1000));
+	protected int WaitTime => 2000 + ((int)(GD.Randf() * 1000));
 
 	private StandardMaterial3D _material;
 
 	public override void _Ready()
 	{
-		var label = GetChild<Label3D>(1);
-		label.Text = Name;
-
 		_material = new StandardMaterial3D();
 
-		var mesh = GetNode<MeshInstance3D>("./Box");
+		var mesh = GetChild<MeshInstance3D>(0);
 		mesh.MaterialOverride = _material;
+
+		var label = GetChild<Label3D>(1);
+		label.Text = Name;
 	}
 
 	protected void SetInitializing() => _material.AlbedoColor = initializingColor;
--- godot-demo/Initializables/A.cs
+++ godot-demo/Initializables/A.cs
@@ -4,8 +4,6 @@ using Scott.Cab.Initialization;
 // [Initializable]
 public partial class A : DemoNode
 {
-    protected override int InitialWaitTimeInMilliSecs => 1250;
-
     // protected virtual partial async Task OnInitialize()
 	// {		
 	// 	SetInitializing();
